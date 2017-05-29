cf3e1832e5ac80b4fdbfa714960e40b600e94fe6
David Andersson <lejonmanen@gmail.com>
2017-05-29 23:46:12 +0200

    React component, Jest and Enzyme

---
+++ reactDatabaseComponent.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+class ModifyItem extends Component {
+	/*
+	Mock these functions using Jest and the Enzyme shallow function instance.
+	let mockFun1 = jest.fn();
+	shallow(<App/>).instance().addItem = mockFun1;
+	*/
+	addItem(itemObj) {
+		throw new Error('Mock this function');
+	}
+	modifyItem(oldItem, newItem) {
+		throw new Error('Mock this function');
+	}
+	deleteItem(itemname) {
+		throw new Error('Mock this function');
+	}
+	
+	/*
+	The functions onAddItem, onModifyItem and onDeleteItem are not completed. But they don't have to be. You are allowed to write a better implementation, but you don't need to do that to finish the exercise.
+	Use the Enzyme shallow function "instance" to call them. Example:
+	shallow(<App/>).instance().onAddItem({type: 'click'});
+	*/
+	onAddItem(event) {
+		this.addItem({name: 'wheelchair', price: 5000});
+	}
+	onModifyItem(event) {
+		this.modifyItem({name: 'rocket', price: 100000000}, {name: 'shuttle', price: 90000000});
+	}
+	onDeleteItem(event) {
+		this.deleteItem('grapefruit');
+	}
+	
+	render() {
+		// TODO: add event handlers using onClick
+		// You don't need to 
+		return (
+			<div>
+				<input type="text" value="item1-name"/>
+				<input type="text" value="item1-price"/>
+				<button>Add new item</button> <br/>
+				<button>Modify price for existing item</button> <br/>
+				<button>Delete item</button> <br/>
+			</div>
+		)
+	}
+}
+
+//export default ......
