8f44e74039943a8cae364e01733ac9b1d9fee008
David Andersson <lejonmanen@gmail.com>
2017-05-31 23:19:03 +0200

    newsletterManager modified to be about mocking

--- newsletterManager.js
+++ newsletterManager.js
@@ -1,17 +1,39 @@
 // Assume mailing list is empty from the start
 class NewsletterManager {
-
+	constructor() {
+		this.db = new NewsletterDatabase();
+	}
+	
 	// returns true if user was added to firebase
 	// throws Error('Already subscribed') if user already subscribed
 	// return false if invalid name or emailAddress
 	subscribeUser(name, emailAddress) {
-		throw new Error('Not implemented yet');
+		// this implementation is probably not going to pass testing...
+		this.db.addUser(name, emailaddress);
 	}
 	
 	// returns true if user was unsubscribed from firebase
 	// throws Error('Not subscribed') if user not subscribed
 	// return false if invalid emailAddress
 	unsubscribeUser(emailAddress) {
-		throw new Error('Not implemented yet');
+		// this implementation is probably not going to pass testing...
+		this.db.removeUser(name, emailaddress);
 	}
+}
+
+// You are not allowed to change this class in this exercise
+// Mock it instead!
+class NewsletterDatabase {
+	addUser(name, emailaddress) {
+		throw new Error('You should not see this when testing');
+	}
+	
+	doesUserExist(emailAddress) {
+		throw new Error('You should not see this when testing');
+	}
+
+	removeUser(emailAddress) {
+		throw new Error('You should not see this when testing');
+	}
+	
 }
